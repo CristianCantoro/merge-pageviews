9acb6c9e8945e0941dc9c8853d6096b071b3a993
Cristian Consonni <cristian@balist.es>
2018-05-27 03:30:51 +0200

    Add usage function

--- process_pageviews_streaming.sh
+++ process_pageviews_streaming.sh
@@ -29,3 +29,114 @@ if ! $SOURCED; then
   set -euo pipefail
   IFS=$'\n\t'
 fi
+
+# usage: merge_pageviews_sorted_by_time_streaming.py [-h]
+#                                                    [--outputdir OUTPUTDIR]
+#                                                    [--encoding ENCODING]
+#                                                    {day,list} ...
+#
+# Merge Wikipedia's pagecounts-raw to get pagecounts-ez.
+#
+# optional arguments:
+#   -h, --help            show this help message and exit
+#   --outputdir OUTPUTDIR
+#                         Where the directory with the elaborated data will be
+#                         saved [default: '.'].
+#   --encoding ENCODING   Encoding of input files [default: 'utf-8'].
+#
+# subcommands:
+#   valid subcommands
+#
+#   {day,list}            additional help
+#
+# usage: merge_pageviews_sorted_by_time_streaming.py day [-h]
+#                                                        [--datadir DATADIR]
+#                                                        [--basename BASENAME]
+#                                                        [--extension EXTENSION]
+#                                                        <date>
+#
+# positional arguments:
+#   <date>                Date to process (avalilable formats: YYYYMMDD or YYYY-
+#                         MM-DD).
+#
+# optional arguments:
+#   -h, --help            show this help message and exit
+#   --datadir DATADIR     Path where the pagecount files are located [default:
+#                         '.'].
+#   --basename BASENAME   Path where the pagecount files are located [default:
+#                         'pagecounts-'].
+#   --extension EXTENSION
+#                         Extension of the pagecount files[default: '.gz'].
+function usage() {
+  (>&2 echo \
+"Usage:
+  process_pageviews_streaming.sh [options]
+
+Sync date on the machine with time from google.com.
+
+Options:
+   --outputdir OUTPUTDIR
+                         Where the directory with the elaborated data will be
+                         saved [default: '.'].
+   --encoding ENCODING   Encoding of input files [default: 'utf-8'].
+
+   --datadir DATADIR     Path where the pagecount files are located [default:
+                         '.'].
+   --basename BASENAME   Path where the pagecount files are located [default:
+                         'pagecounts-'].
+   --extension EXTENSION
+                         Extension of the pagecount files[default: '.gz'].
+
+  -h, --help             Show this help and exits.
+
+Example:
+  ssh-fingerprint localhost")
+}
+
+function cli_args() {
+  while getopts ":oebdxh" opt; do
+    case $opt in
+      o)
+        (>&2 echo "-o was triggered, Parameter: $OPTARG")
+        ;;
+      e)
+        (>&2 echo "-e was triggered, Parameter: $OPTARG")
+        ;;
+      b)
+        (>&2 echo "-b was triggered, Parameter: $OPTARG")
+        ;;
+      d)
+        (>&2 echo "-d was triggered, Parameter: $OPTARG")
+        ;;
+      x)
+        (>&2 echo "-x was triggered, Parameter: $OPTARG")
+        ;;
+      h)
+        usage
+        exit 0
+        ;;
+      \?)
+        (>&2 echo "Invalid option: -$OPTARG")
+        exit 1
+        ;;
+      :)
+        (>&2 echo "Option -$OPTARG requires an argument.")
+        exit 1
+        ;;
+      *)
+        (>&2 echo "Flag $opt not recognized." )
+        exit 1
+        ;;
+    esac
+  done
+}
+
+
+function main() {
+
+  cli_args "$@"
+
+}
+
+
+main "$@"
